498528bc02353eb1ed43bcc049a847dfbae2b2d6
Constantin Luchian <lcmanagernext@gmail.com>
2025-04-02 16:34:55 +0200

    Added category navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CatnavigationComponent } from './catnavigation/catnavigation.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, CatnavigationComponent],
   imports: [BrowserModule, FontAwesomeModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
