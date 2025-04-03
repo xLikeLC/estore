89a19cac06b06883216c79163ac3fa06c272d46b
Constantin Luchian <lcmanagernext@gmail.com>
2025-04-03 14:14:05 +0200

    Added 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {NotfoundComponent} from "./notfound/notfound.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: '', redirectTo:'/home', pathMatch:'full'}
+  {path: '', redirectTo:'/home', pathMatch:'full'},
+  {path:'**', component:NotfoundComponent}
 ];
 
 @NgModule({
