95e01ddf4b3ae5615c8fa8ca2ead9b19869d1aa3
Constantin Luchian <lcmanagernext@gmail.com>
2025-04-03 13:50:23 +0200

    Created home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CatnavigationComponent } from './catnavigation/catnavigation.component';
+import { HomeComponent } from './home/home.component';
+import { HomeModule } from "./home/home.module";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, CatnavigationComponent],
-  imports: [BrowserModule, FontAwesomeModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent, CatnavigationComponent, HomeComponent],
+  imports: [BrowserModule, FontAwesomeModule, AppRoutingModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
