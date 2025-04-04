753ea4f4ffc310bc4d39f7a8f60cda8fb7777d85
Constantin Luchian <lcmanagernext@gmail.com>
2025-04-04 13:10:09 +0200

    Fetching data into side navigation

--- src/app/home/components/sidenavigation/sidenavigation.component.ts
+++ src/app/home/components/sidenavigation/sidenavigation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Category} from "../../types/category.type";
+import {CategoryService} from "../../services/category.service";
 
 @Component({
   selector: 'app-sidenavigation',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidenavigation.component.scss']
 })
 export class SidenavigationComponent {
+   categories: Category[] = [];
+   constructor(categoryService: CategoryService) {
+     this.categories = categoryService.getAllCategories()
+   }
 
+   getCategories (parentCategoryId?: number): Category[] {
+     return this.categories.filter(
+       (category) => category.parent_category_id === parentCategoryId
+     );
+   }
 }
---
+++ src/app/home/services/category.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {Category} from "../types/category.type";
+import {categories} from "../sampleData/categories.data";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor() { }
+
+  getAllCategories(): Category[] {
+    return categories
+  }
+}
